bde5bac11788a9a902f3c175b38d6b1764f5546c
Kevin Cohen <KevinCohen@Kevins-MacBook-Pro.local>
2015-11-30 18:20:54 -0500

    Bishop functionality, pawn tower in blender, check if object is in path of piece

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -6,13 +6,19 @@ using System.Collections;
 public class Piece : MonoBehaviour {
 
 	int  [] pawns = new int[8];
+
+	
+
 	
 	bool pawnSelected = false;
 	bool towerSelected = false;
-	int pawnNumber ;
+	bool bishopSelected = false;
+
+	int pawnNumber;
 
 	GameObject pawn;
 	GameObject tower;
+	GameObject bishop;
 
 	
 	float tileZ = 0;
@@ -32,12 +38,61 @@ public class Piece : MonoBehaviour {
 		   	   		g.transform.tag = "tile";			// Me daba como que fastidio set todas a "tile"
 		   	   }
 		  }
-		//MainClass.getBoard(boardAcquired);
 
 	
 	// The pawn array will serve as ocurrences. All will have 0. If one has 1 then that means it was selected and moves so that pawn can't move two tiles.
 	}
-	
+	///*
+/*
+	object [] findObjectsInterferingPawn()
+	{
+		object[] objects;
+
+
+
+		return objects;
+	}
+	*/
+
+	// FOR ENEMIES, Check if i can find a gameobject in a given coordinate.
+
+
+	// WORKS for tower and pawn. It handles vertical and horizontal movement.(Add it to the tower piece when implemented)
+	// Will create another method for diagonal as well for bishop and queen. Horse will be the only special piece
+	bool checkGameObjectPath(float tilePositionZ, float pawnPositionZ, float pawnPositionX)
+	{
+		object[] obj = GameObject.FindObjectsOfType(typeof (GameObject));	
+
+		foreach(object o in obj)
+		{
+		    GameObject g = (GameObject) o;
+
+		  	//Its the same object dont include it
+			
+			  
+			   if(g.transform.tag != "tile" && g.transform.tag !="MainCamera")		// If its not a tile then it is a game object, check to see if its on the way
+			   {
+			   		if(!(pawnPositionX == g.transform.position.z && g.transform.position.z == pawnPositionZ))
+			   	   	{
+			   	   		
+			   	   		if(pawnPositionX == g.transform.position.x)		// If the piece is on the same x axis (same column per say)
+			   	   		{												// Then check if its in between the tile selected and the pawn's z position.
+			   	   			if(g.transform.position.z <= tilePositionZ && g.transform.position.z>pawnPositionZ)
+			   	   			{
+			   	   				//Debug.Log("Epale");
+			   	   				return true;
+			   	   			}
+			   	   		}
+			   	   	}
+			   }
+			
+
+		}
+		//console.log("Epale");
+		return false;		// False --> good to go, no game object (piece) in the path of the desired move
+							// True --> There is a game object in the pawn's path, handle it when moving the pawn.
+	}
+	//*/
 	// Update is called once per frame
 	void Update () {
 
@@ -52,8 +107,8 @@ public class Piece : MonoBehaviour {
 		{
 			// Select the tile that you want to use. Display that in a text, for now in debug	
 			towerSelected = false;
-
 			pawnSelected = true;
+			bishopSelected = false;
 
 			pawnNumber = int.Parse(hit.transform.name.Substring(4)); 	// so pawn 0 was selected or pawn 1 or whatever was selected
 			pawn = hit.transform.gameObject;
@@ -76,27 +131,43 @@ public class Piece : MonoBehaviour {
 			// Select the tile that you want to use. Display that in a text, for now in debug	
 			pawnSelected = false;
 			towerSelected = true;
+			bishopSelected = false;
+
 			tower = hit.transform.gameObject;
-			
-			//boardAcquired = MainClass.getBoard();
-			//Debug.Log(boardAcquired[0][0]);
+		
 
 			Debug.Log("Select the tile that you would like to move this " + hit.transform.name.Substring(0,5));
 
 
+		}
 
-			// Now player will click a tile. Check if the tile click is valid for the corresponding character move, in this case, the tower.
-			// CheckifValid(Tile t, tower p)	// This will get the current position of tower P in the array, check if this + 1 or + 2 in the vertical axis is what the tile was clicked. False otherwise
+		if(hit.transform.tag == "bishop")	
+		{
+			// Select the tile that you want to use. Display that in a text, for now in debug	
+			towerSelected = false;
+			pawnSelected = false;
+			bishopSelected = true;
+
+			bishop = hit.transform.gameObject;
+			
+		
+			Debug.Log("Select the tile that you would like to move this " + hit.transform.name.Substring(0,6));
+
+
+
+			// Now player will click a tile. Check if the tile click is valid for the corresponding character move, in this case, the pawn.
+			// CheckifValid(Tile t, Pawn p)	// This will get the current position of pawn P in the array, check if this + 1 or + 2 in the vertical axis is what the tile was clicked. False otherwise
 			// If it is true, move and update array.
-			// move(tower p)?
+			// move(Pawn p)?
+
 		}
 		else if(hit.transform.tag == "tile")
 		{
 
 			if(pawnSelected){		
-				Debug.Log(pawnNumber);
+				//Debug.Log(pawnNumber);
 
-				Debug.Log("Current selection of current pawn in array is:" + pawns[pawnNumber]);
+				//Debug.Log("Current selection of current pawn in array is:" + pawns[pawnNumber]);
 				pawns[pawnNumber]++;
 
 				if(pawns[pawnNumber] > 1)
@@ -107,8 +178,12 @@ public class Piece : MonoBehaviour {
 				tileX = hit.transform.position.x;
 				// Handles: pawn can only move one up in the x direction (based on our unity setup). Can move 2 tiles up on the first try (will handle that later). Can't move back. And can't move to the sides.
 
-				if((tileZ - pawn.transform.position.z <=1 || (tileZ - pawn.transform.position.z <=2 && !pawnMovedFirstTime)) && tileX == pawn.transform.position.x && tileZ >= pawn.transform.position.z)
+				
+				// Now check if there was another game object in the pawn's way (another piece in front of the pawn)
+				if((tileZ - pawn.transform.position.z <=1 || (tileZ - pawn.transform.position.z <=2 && !pawnMovedFirstTime)) && tileX == pawn.transform.position.x && tileZ >= pawn.transform.position.z && !checkGameObjectPath(tileZ,pawn.transform.position.z,pawn.transform.position.x))
 				{									// If I selected a pawn, check to see if the selected tile is valid for pawn.
+					
+					
 					pawn.transform.position = new Vector3(pawn.transform.position.x,pawn.transform.position.y,tileZ);
 					pawnSelected = false;
 					pawnMovedFirstTime = true;
@@ -117,6 +192,7 @@ public class Piece : MonoBehaviour {
 				{
 					Debug.Log("Sorry, not a valid move for Pawn");
 					pawnSelected = false;
+					pawns[pawnNumber]--;
 				}
 
 				pawnMovedFirstTime = false;
@@ -127,8 +203,8 @@ public class Piece : MonoBehaviour {
 				tileX = hit.transform.position.x;
 
 				// Handles tower movement (only in x or z direction)
-				if(tower.transform.position.x == tileX || tower.transform.position.z == tileZ)
-				{									// If I selected a pawn, check to see if the selected tile is valid for pawn.
+				if((tower.transform.position.x == tileX || tower.transform.position.z == tileZ) && !checkGameObjectPath(tileZ,tower.transform.position.z,tower.transform.position.x))
+				{									
 					tower.transform.position = new Vector3(tileX,tower.transform.position.y,tileZ);
 					towerSelected = false;
 				
@@ -137,6 +213,24 @@ public class Piece : MonoBehaviour {
 				{
 					Debug.Log("Sorry, not a valid move for Tower");
 					towerSelected = false;
+				}
+			}
+			else if(bishopSelected)
+			{
+				tileZ = hit.transform.position.z;
+				tileX = hit.transform.position.x;
+
+				// Handles tower movement (only in x or z direction)
+				if(tileX != bishop.transform.position.x && tileZ != bishop.transform.position.z)
+				{									
+					bishop.transform.position = new Vector3(tileX,bishop.transform.position.y,tileZ);
+					bishopSelected = false;
+				
+				}
+				else
+				{
+					Debug.Log("Sorry, not a valid move for Bishop");
+					bishopSelected = false;
 				}
 			}
 			else
